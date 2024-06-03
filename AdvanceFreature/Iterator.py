a1516e3196949193a08b04c99e6a46c08da7e209
zhourui <zhourui@pxsemic.com>
2024-06-03 15:56:57 +0800

    代码格式化

--- AdvanceFreature/Iterator.py
+++ AdvanceFreature/Iterator.py
@@ -3,18 +3,15 @@ from collections.abc import Iterable
 
 l = list(range(20))
 
-for e in l :
+for e in l:
     print(e)
 
-
-d = {"a":1, "b":2, "c":3}
-
-for key in d :
+d = {"a": 1, "b": 2, "c": 3}
+for key in d:
     print(key)
 
 for value in d.values():
     print(value)
-
 
 s = "abc"
 for c in s:
@@ -27,18 +24,18 @@ print(isinstance(l, Iterable))
 print(isinstance(d, Iterable))
 print(isinstance(s, Iterable))
 
-
 ## 同时获取索引下表和内容，使用 enumerate()函数，将list变成 索引-元素对
-for i, value in enumerate(['a','b', 'c']):
+for i, value in enumerate(['a', 'b', 'c']):
     print(i, value)
 
 ## for循环同时引用两个变量
-for b1, a1 in [(1,2),(3,4)]:
+for b1, a1 in [(1, 2), (3, 4)]:
     print(a1, b1)
+
 
 ## 请使用迭代查找一个list中最小和最大值，并返回一个tuple
 def getMaxMin(tl):
-    if not tl: # if tl == [] || if len(tl) == 0
+    if not tl:  # if tl == [] || if len(tl) == 0
         return (None, None)
     max = tl[0]
     min = tl[0]
@@ -49,6 +46,7 @@ def getMaxMin(tl):
         if e < min:
             min = e
     return (min, max)
+
 
 tl = list(range(100))
 print(getMaxMin([7]))
