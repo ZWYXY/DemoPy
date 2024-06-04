1650ebf668ccaf17bccc6d32d05412d7bff57d30
zhourui <zhourui@pxsemic.com>
2024-06-04 15:46:11 +0800

    修改LIST和SET注释

--- _01BaseGrammar/Collectors.py
+++ _01BaseGrammar/Collectors.py
@@ -1,24 +1,12 @@
 # About List
-list = [1, 2, 3, 4, 5]
-list.append(7)
-list.insert(5, 6)
-list.pop()
-list.pop(0)
+list = [1, 2, 3, 4, '5']
+list.append(7) # 队尾追加元素
+list.insert(5, 6) # 指定位置增加元素
+list.pop() # 删除队尾元素
+list.pop(0) # 删除指定位置元素
 print(len(list))
 print(list)
 
-# About Set
-s = set([1,2,3,4])
-print(s)
-s.add(5)
-print(s)
-s.remove(5)
-print(s)
-
-s1 = set([3, 4])
-print(s1)
-print(s1&s) # 求交集
-print(s1|s) # 求并集
 
 # About tuple the immutable List
 myTuple = (1, (1,))
--- _01BaseGrammar/Dict.py
+++ _01BaseGrammar/Dict.py
@@ -11,3 +11,16 @@ print(105 in d1)
 print(d1.get(105)) # 不存在就返回None 或 -1
 print(d1.pop(105)) # 删除数据
 print(d1.get(105)) # 不存在就返回None 或 -1
+
+# About Set
+s = set([1,2,3,4])
+print(s)
+s.add(5)
+print(s)
+s.remove(5)
+print(s)
+
+s1 = set([3, 4])
+print(s1)
+print(s1&s) # 求交集
+print(s1|s) # 求并集
