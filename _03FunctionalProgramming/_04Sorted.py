aee5f7bf9eea9c57faaaa3f693f682eefffa3c6a
周睿 <1142165668@qq.com>
2024-06-11 16:38:33 +0800

    Refactor

--- _03FunctionalProgramming/_04Sorted.py
+++ _03FunctionalProgramming/_04Sorted.py
@@ -10,12 +10,20 @@ print(res)
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 res = sorted(L, key=itemgetter(0))
 res = sorted(L, key=itemgetter(1), reverse=True)
+
+
 def by_name(t):
     return t[0]
+
+
 def by_score(t):
     return t[1]
-def fn(t): # by_score一种特殊写法
+
+
+def fn(t):  # by_score一种特殊写法
     return -t[1]
+
+
 res = sorted(L, key=by_name)
 res = sorted(L, key=fn)
 print(res)
